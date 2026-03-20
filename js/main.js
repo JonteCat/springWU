76a541f6d723523f634a749337fdf79bb7e768c8
JonteCat <jontemyra2008@gmail.com>
2026-03-20 14:27:17 +0100

    2026-03-20

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let button = document.querySelector("menu-button");
+button.addEventListener("click", showMenu);
+let menu = document.querySelector("nav.menu");
+menu.addEventListener("")
+
+function showMenu() {
+    menu.classList.toggle("show");
+
+}
